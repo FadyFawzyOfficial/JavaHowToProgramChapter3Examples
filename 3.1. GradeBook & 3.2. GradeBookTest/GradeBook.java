4cfd94c7c2a1454d5e918bdde82cda19c79c0517
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2017-01-11 17:07:32 +0200

    3.1. GradeBook

---
+++ 3.1. GradeBook & 3.2. GradeBookTest/GradeBook.java
@@ -0,0 +1,11 @@
+// Fig. 3.1: GradeBook.java Author: Eng.Fady Fawzy
+// Class declaration with one method.   Created: 31/8/2016
+public class GradeBook 
+{
+    // display a welcome message to the GradeBook user
+    public void displayMessage()
+    {
+        System.out.println( "Welcome to the Grade Book!" );
+        System.out.println( "Welcome to My First Class!" );
+    } // end method displayMessage
+} // end class GradeBook
