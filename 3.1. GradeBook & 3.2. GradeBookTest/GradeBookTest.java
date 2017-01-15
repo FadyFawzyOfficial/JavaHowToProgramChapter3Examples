07f1d189fedbde81dc56ab0857418c50b8adbe9b
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2017-01-15 17:08:27 +0200

    3.2. GradeBookTest

---
+++ 3.1. GradeBook & 3.2. GradeBookTest/GradeBookTest.java
@@ -0,0 +1,14 @@
+// Fig. 3.2: GradeBookTest.java Author: Eng.Fady Fawzy
+// Creating a GradeBook object and calling its displayMessage method.   Created: 31/8/2016
+public class GradeBookTest
+{
+    // main method begins program execution
+    public static void main( String[] args )
+    {
+        // create a GradeBook object and assign it to myGradeBook
+        GradeBook myGradeBook = new GradeBook();
+        
+        // call myGradeBook's display message method
+        myGradeBook.displayMessage();
+    } // end main
+} // end class GradeBookTest
