c55e6b3f1c45a8f0c41d0b4f22908100f5008a2a
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2017-02-20 17:22:59 +0200

    3.17. Dialog1

---
+++ 3.17. Dialog1/Dialog1.java
@@ -0,0 +1,12 @@
+// Fig. 3.17: Dialog1.java  Author: Eng.Fady Fawzi  Created: 13/9/2016
+// Using JOptionPane to display multiple lines in a dialog box.
+import javax.swing.JOptionPane;
+
+public class Dialog1 
+{
+    public static void main( String[] args )
+    {
+        // display a dialog with a message
+        JOptionPane.showMessageDialog( null, "Welcome\nto\nJava" );
+    } // end main
+} // end class Dialog1
